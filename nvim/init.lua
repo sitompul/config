755f563a77607b13d392e92b20261f1384fe3a9c
Elfino Sitompul <elfinositompul@gmail.com>
2023-03-20 15:50:16 +0700

    add comments support

--- nvim/init.lua
+++ nvim/init.lua
@@ -196,9 +196,6 @@ require('packer').startup(function(user)
             -- REQUIRED - you must specify a snippet engine
             expand = function(args)
                 vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
             end
         },
         window = {
@@ -238,9 +235,6 @@ require('packer').startup(function(user)
         }, {
             name = 'vsnip'
         } -- For vsnip users.
-        -- { name = 'luasnip' }, -- For luasnip users.
-        -- { name = 'ultisnips' }, -- For ultisnips users.
-        -- { name = 'snippy' }, -- For snippy users.
         }, {{
             name = 'buffer'
         }})
@@ -303,6 +297,14 @@ require('packer').startup(function(user)
     -- Auto pairs and autocomplete integration
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+    -- Comment
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 end)
 
 -- Vanilla Binding
