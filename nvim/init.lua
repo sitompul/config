ae89d5f107056f6c15d1b9d272101ff4c10662d6
Elfino Sitompul <elfinositompul@gmail.com>
2026-02-06 05:57:52 +0700

    Remove lualine setup and dependency
    Removed lualine configuration and its dependency from init.lua.

--- nvim/init.lua
+++ nvim/init.lua
@@ -109,11 +109,6 @@ require("lazy").setup({
       -- "rcarriga/nvim-notify",
       }
   },
-  -- Lualine is required since the new UI will disable the UI for VIM mode.
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
   -- Show code alignment like in modern IDE.
   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
   'RRethy/vim-illuminate',
@@ -267,45 +262,7 @@ require('vscode').setup({
 })
 require("vscode").load()
 
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'vscode',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      'NvimTree'
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+
 require("noice").setup({
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
