14a2a58f28d800acb7768a9257537bd6d80ff218
efs <elfinositompul@gmail.com>
2026-02-10 22:30:31 +0700

    remove unused

--- deps.sh
+++
@@ -1,1 +0,0 @@
-sudo dnf install -y kitty golang-go g++ clang clang-tools-extra bear cmake ninja-build ocular transmission neovim docker ghostwriter obs-studio mpv steam epiphany chromium git vim-common skim ripgrep tmux unrar virt-manager java-latest-openjdk fsearch
